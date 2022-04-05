767c463e21fcb4bcd68076a89f62478c54e5a6f0
leo spinola <leonardospinola22@gmail.com>
2022-04-05 00:46:11 -0300

    otimizando um pouco o codigo

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 const genius = document.querySelector(".genius");
+
 function criaJogo(){
     let lentes__container = document.createElement('div');
     lentes__container.setAttribute('class','lentes__container');
     lentes__container.innerHTML = `
-     <button class="genius__lentes genius__lentes--azul" id="azul" ></button>
+        <button class="genius__lentes genius__lentes--azul" id="azul" ></button>
         <button class="genius__lentes genius__lentes--vermelho" id="vermelho"></button>
         <button class="genius__lentes genius__lentes--verde" id="verde"></button>
         <button class="genius__lentes genius__lentes--amarelo" id="amarelo"></button>
@@ -43,53 +44,16 @@ const cont_botoes = [
 const genius__contador = document.querySelector('.genius__contador')
 const iniciar = document.querySelector('.iniciar')
 
+function criaCores(){
+    return Math.floor(Math.random() * 4);
+} 
 
 function iniciarJogo(){
     arrCores=[];
-    let novaCor = Math.floor(Math.random() * 4)
-    arrCores.push(brilhaLente(cont_botoes,novaCor));
+    arrCores.push(brilhaLente(cont_botoes,criaCores()));
     contador = 0;
     genius__contador.innerText="0"
 }
-
-iniciar.addEventListener('click',iniciarJogo);
-
-
-const click_usuario = document.querySelectorAll('.genius__lentes');
-for(let i = 0;i<click_usuario.length;i++){
-    click_usuario[i].addEventListener('click', (e)=>{
-        console.log(arrCores)
-        console.log(contador)
-     if(e.path[0].id !== arrCores[contador].id){
-        alert("você errou!");     
-        reiniciarJogo();
-        contadorNaTela.innerText=arrCores.length;
-    }
-    else if(e.path[0].id === arrCores[contador].id){
-                if(contador === arrCores.length-1){
-                    let novaCor = Math.floor(Math.random() * 4);
-                    arrCores.push(cont_botoes[novaCor])
-                    let index = 0;
-                    function continuarSequencia(arrSequencia){
-                        if(index === arrSequencia.length-1){
-                                clearInterval(continuarCores);
-                            }
-                            brilhaLente(arrSequencia,index);
-                            index++;
-                        } 
-                    let continuarCores = setInterval(continuarSequencia,1500,arrCores)
-                    index = 0;
-                    contador = 0;
-                    console.log(arrCores)
-                    console.log(contador)
-                }else{
-                    contador++
-                    contadorNaTela.innerText=arrCores.length-1;
-                }
-            } 
-    })
-}
-
 
 function brilhaLente(arrBrilho,num){
     const escolheCor = document.getElementById(arrBrilho[num].id)        
@@ -106,3 +70,36 @@ function reiniciarJogo(){
     contador = 0;
 }
 
+// "Operador do Sistema"
+iniciar.addEventListener('click',iniciarJogo);
+const click_usuario = document.querySelectorAll('.genius__lentes');
+
+for(let i = 0;i<click_usuario.length;i++){
+    click_usuario[i].addEventListener('click', (e)=>{
+        if(e.path[0].id !== arrCores[contador].id){
+            alert("você errou!");     
+            reiniciarJogo();
+            contadorNaTela.innerText=arrCores.length;
+        }
+        else{
+            if(contador === arrCores.length-1){
+                arrCores.push(cont_botoes[criaCores()])
+                let index = 0;
+                function continuarSequencia(arrSequencia){
+                    if(index === arrSequencia.length-1){
+                        clearInterval(continuarCores);
+                    }
+                    brilhaLente(arrSequencia,index);
+                    index++;
+                } 
+                let continuarCores = setInterval(continuarSequencia,1500,arrCores)
+                index = 0;
+                contador = 0;
+            }
+            else{
+                contador++
+                contadorNaTela.innerText=arrCores.length-1;
+            }
+        } 
+    })
+}
