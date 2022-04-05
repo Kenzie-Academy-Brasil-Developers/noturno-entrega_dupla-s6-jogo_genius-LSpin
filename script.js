0e0cbeb7df879c1e5cb1a08b54cded46489ff2ed
leo spinola <leonardospinola22@gmail.com>
2022-04-04 21:47:18 -0300

    mudança de luzes configuradas

--- script.js
+++ script.js
@@ -20,10 +20,6 @@ function criaJogo(){
                 <button class="genius__botao iniciar"></button>
                 <p>Iniciar/Reiniciar</p>
             </div>
-            <div class="agrupar__elementos">
-                <button class="genius__botao melhor__sequencia"></button>
-                <p>Melhor sequência</p>
-            </div>
         </div>
     `
     genius.appendChild(lentes__container)
@@ -56,11 +52,9 @@ function liga(){
 const botao_onOff = document.querySelector('.on__off');
 botao_onOff.addEventListener('click',liga());
 
-
 function iniciarJogo(){
-    let num_cor = Math.floor(Math.random() * 4);
-    arrCores.push(cont_botoes[num_cor]);
-    brilhaLente(arrCores);
+    let novaCor = Math.floor(Math.random() * 4)
+    arrCores.push(brilhaLente(cont_botoes,novaCor));
 }
 
 iniciar.addEventListener('click',iniciarJogo);
@@ -69,16 +63,34 @@ iniciar.addEventListener('click',iniciarJogo);
 const click_usuario = document.querySelectorAll('.genius__lentes');
 for(let i = 0;i<click_usuario.length;i++){
     click_usuario[i].addEventListener('click', (e)=>{
-        console.log(e.path[0].id);
+            if(e.path[0].id === arrCores[contador].id){
+                if(contador === arrCores.length-1){
+                    let novaCor = Math.floor(Math.random() * 4);
+                    arrCores.push(cont_botoes[novaCor])
+                    let index = 0;
+                    function continuarSequencia(arrSequencia){
+                        if(index === arrSequencia.length-1){
+                                clearInterval(continuarCores);
+                            }
+                            brilhaLente(arrSequencia,index);
+                            index++;
+                        } 
+                    let continuarCores = setInterval(continuarSequencia,3000,arrCores)
+                    index = 0;
+                    console.log(arrCores)
+                }
+            } 
     })
 }
 
 
-function brilhaLente(arrBrilho){
-    const escolheCor = document.getElementById(arrBrilho[contador].id)        
+function brilhaLente(arrBrilho,num){
+    const escolheCor = document.getElementById(arrBrilho[num].id)        
     escolheCor.classList.add('genius__lentes--ligado');
     function apagaLente(){
         escolheCor.classList.remove('genius__lentes--ligado');
     }
     setTimeout(apagaLente, 2000);
+    return arrBrilho[num];
 }
+
